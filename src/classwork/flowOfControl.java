033caceef7ee8031ff23714c8f636e73cefb3cc3
eddy <eddyv5413@gmail.com>
2016-09-12 16:59:14 -0400

    flow of control

---
+++ src/classwork/flowOfControl.java
@@ -0,0 +1,28 @@
+package classwork;
+
+public class flowOfControl {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		
+		
+		if (1-1 == 0 &&"".isEmpty()){//this is a boolean value  it will not print anything because a space is a string 
+			System.out.println("name");
+		}
+		
+		if (1-1 == 0){
+			System.out.println("Hello");
+			System.out.println("wow");
+		}
+		
+		String a = "Hello";
+		String b = "Hello";
+		
+		if (a.equals(b)){ //comparing two strings 
+			System.out.println("Hello");
+		}
+		else
+			System.exit(0);
+	}
+
+}
